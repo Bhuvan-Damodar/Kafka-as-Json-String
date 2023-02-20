589b8ddb5e2946a1872527d552efbf9930d6e4b1
Bhuvan A <bhuvan.a@happiestminds.com>
2023-02-20 16:40:06 +0530

    intial commit

---
+++ src/main/java/com/kafka/learn/kafkaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class kafkaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(kafkaSpringBootApplication.class, args);
+	}
+
+}
